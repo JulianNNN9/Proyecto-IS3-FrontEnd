2885a32a1142ac13444dcfc116fbe46683d29558
Nicolás Jurado <99767358+NicholashiG@users.noreply.github.com>
2025-03-09 16:25:24 -0500

    Ya se puede editar el perfil

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,20 +1,29 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 import { QuejaSugerenciaService } from './services/queja-sugerencia.service';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { ReporteService } from './services/reporte.service';
+import { usuarioInterceptor } from './interceptor/usuario.interceptor';
+import { AdminService } from './services/admin.service';
+import { TokenService } from './services/token.service';
+import { PublicoService } from './services/publico.service';
+import { ClienteService } from './services/cliente.service';
+import { AuthService } from './services/auth.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), 
     provideHttpClient(
       withFetch(),
-    ),
-    provideClientHydration(),
-    QuejaSugerenciaService,
-    ReporteService
+      withInterceptors([usuarioInterceptor])),
+    AdminService,
+    TokenService,
+    ReporteService,
+    PublicoService,
+    ClienteService,
+    AuthService,
+    QuejaSugerenciaService, provideAnimationsAsync()
   ]
 };
